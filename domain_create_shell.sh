e266034c300877a42fc496ae95d01392d96185ae
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2024-02-06 14:16:39 +0800

    Create domain_create_shell.sh

---
+++ domain_create_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Peter Yau
+# E-mail: drpeteryau@gmail.com
+# Description: create virtualmin doamin from csv file
+# Configs: [default_password], [plan_name]
+# ------------------------------------------
+INPUT=$1
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read domain
+do
+	virtualmin create-domain --domain $domain --pass "[default_password]" --plan "[plan_name]" --unix --dir --webmin --limits-from-plan
+done < $INPUT
+IFS=$OLDIFS
